a827dae0828830eb75379717c0eac2667c5d8658
mukundgupta <70640082+mukundgupta@users.noreply.github.com>
2020-10-08 16:51:08 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,23 @@
+class Paper {
+  constructor(x,y,diameter){
+
+      var options = {
+          isStatic: false,
+          restitution: 0.5,
+          friction:1,
+          density: 1.2
+      }
+      this.diameter = diameter
+      this.body = Bodies.circle(x,y,diameter,options)
+      World.add(world, this.body);
+ 
+  }
+  display(){
+      push();
+      translate(this.body.position.x, this.body.position.y);
+      fill("white")
+      ellipseMode(RADIUS)
+      ellipse(0,0,this.diameter/2,this.diameter/2)
+      pop();
+  }
+}
---
+++ sketch.js
@@ -0,0 +1,50 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var paper;
+var ground;
+var rect1,rect2,rect3;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+	engine = Engine.create();
+	world = engine.world;
+ 
+	ground = new Ground(400,height,1000,21)
+rect1 = new Dustbin(600,630,20,100)
+rect2 = new Dustbin(750,630,20,100)
+rect3 = new Dustbin(675,675,173,20)
+	paper = new Paper(150,500,50);
+	//Create the Bodies Here.
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  
+  background(0);
+  Engine.update(engine);
+  paper.display();
+  ground.display();
+  rect1.display();
+  rect2.display();
+  rect3.display();
+  drawSprites();
+ 
+}
+
+function keyPressed(){
+	if (keyCode === UP_ARROW){
+		Matter.Body.applyForce(paper.body,paper.body.position,{x: 300,y: -500})
+	}
+}
+
